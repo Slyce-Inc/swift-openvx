578c72e2ea63fd25f287790edc81538aa5a9c677
Jason Allum <jallum@tonicdesign.com>
2018-08-08 08:27:23 -0400

    Added machinery to simplify the creation of images in host memory.

--- Sources/OpenVX/Image.swift
+++ Sources/OpenVX/Image.swift
@@ -3,24 +3,44 @@ import Clibvisionworks
 
 public class Image: Referenceable, Imageable {
   public let reference: vx_image
+  private let ptrs: [UnsafeMutableRawPointer?]?
 
   public init(reference: vx_image) {
     vxRetainReference(reference)
     self.reference = reference
+    self.ptrs = nil
   }
 
-  public init?(context: Context, width: Int, height: Int, type: ImageType) {
+  public init?(context: Context, width: Int, height: Int, type: ImageType, memoryType: MemoryType = .None) {
     if type == .Virtual {
-      return nil
+      fatalError("OpenVX.Image should never be created with type .Virtual")
     }
-    guard let reference = vxCreateImage(context.reference, vx_uint32(width), vx_uint32(height), type.vx_value) else {
-      return nil
+    switch memoryType {
+      case .None:
+        guard let reference = vxCreateImage(context.reference, vx_uint32(width), vx_uint32(height), type.vx_value) else {
+          return nil
+        }
+        self.reference = reference
+        self.ptrs = nil
+
+      case .Host:
+        let addrs = type.vx_imagepatch_addressing(width: width, height:height)
+        guard let ptrs = type.allocateMemory(width:width, height:height) else {
+          return nil
+        }
+        guard let reference = vxCreateImageFromHandle(context.reference, type.vx_value, addrs, ptrs, memoryType.vx_value) else {
+          return nil
+        }
+        self.ptrs = ptrs
+        self.reference = reference
     }
-    self.reference = reference
   }
 
   deinit {
     vxReleaseImage(reference)
+    if let ptrs = self.ptrs {
+      free(ptrs[0])
+    }
   }
 
   public func create(fromROI rectangle: Rectangle) -> Image? {
@@ -44,5 +64,73 @@ public extension Image {
     defer { vxCommitImagePatch(self.reference, nil, vx_uint32(plane), &addr, base) }
     let x = block(addr, base!)
     return x
+  }
+}
+
+private extension ImageType {
+  func vx_imagepatch_addressing(width:Int, height:Int) -> [vx_imagepatch_addressing_t] {
+    switch self {
+      case .U8:
+        return [
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width), dim_y: vx_uint32(height), stride_x: 1, stride_y: vx_int32(width), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0)
+        ]
+      case .NV12:
+        return [
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width), dim_y: vx_uint32(height), stride_x: 1, stride_y: vx_int32(width), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0),
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width / 2), dim_y: vx_uint32(height / 2), stride_x: 2, stride_y: vx_int32(width), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0)
+        ]
+      case .IYUV:
+        return [
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width), dim_y: vx_uint32(height), stride_x: 1, stride_y: vx_int32(width), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0),
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width / 2), dim_y: vx_uint32(height / 2), stride_x: 1, stride_y: vx_int32(width / 2), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0),
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width / 2), dim_y: vx_uint32(height / 2), stride_x: 1, stride_y: vx_int32(width / 2), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0)
+        ]
+      case .RGB:
+        return [
+          vx_imagepatch_addressing_t(dim_x: vx_uint32(width), dim_y: vx_uint32(height), stride_x: 3, stride_y: vx_int32(width * 3), scale_x: 0, scale_y: 0, step_x: 0, step_y: 0)
+        ]
+      default:
+        preconditionFailure("ImageType (\(self)) is unimplemented")
+    }
+  }
+}
+
+private extension ImageType {
+  func allocateMemory(width:Int, height:Int) -> [UnsafeMutableRawPointer?]? {
+    switch self {
+      case .U8:
+        guard let ptr = malloc(width * height) else {
+          return nil
+        }
+        return [ptr]
+
+      case .NV12:
+        guard let ptr = malloc((width * height) + (width * (height >> 1))) else {
+          return nil
+        }
+        return [
+          ptr,
+          ptr + (width * height)
+        ]
+
+      case .IYUV:
+        guard let ptr = malloc((width * height) * 2) else {
+          return nil
+        }
+        return [
+          ptr,
+          ptr + (width * height),
+          ptr + (width * height) + (width / 2 * height / 2),
+        ]
+
+      case .RGB:
+        guard let ptr = malloc(width * height * 3) else {
+          return nil
+        }
+        return [ptr]
+
+      default:
+        preconditionFailure("ImageType (\(self)) is unimplemented")
+    }
   }
 }
