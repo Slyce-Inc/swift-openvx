97e48b9493a483c525f4dc7d62d9516722023f36
Jason Allum <jallum@tonicdesign.com>
2018-08-08 07:05:21 -0400

    Added vxCreateImage

---
+++ Sources/OpenVX/vxCreateImage.swift
@@ -0,0 +1,9 @@
+import Clibvisionworks
+
+
+public func vxCreateImage(_ context: Context, _ width: Int, _ height: Int, _ type: ImageType) -> Image? {
+  guard let reference = vxCreateImage(context.reference, vx_uint32(width), vx_uint32(height), type.vx_value) else {
+    return nil
+  }
+  return Image(reference:reference)
+}
