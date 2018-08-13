db72ca787118c53fb035831e4abad1b8324e2c06
Jason Allum <jallum@tonicdesign.com>
2018-08-13 16:13:38 -0400

    Improved the interface to vxSwapImageHandle()

--- Sources/OpenVX/vxSwapImageHandle.swift
+++ Sources/OpenVX/vxSwapImageHandle.swift
@@ -1,18 +1,19 @@
 import Clibvisionworks
 
 
-public func vxSwapImageHandle(_ image: Image, _ new_ptrs: [UnsafeMutableRawPointer?]?, _ prev_ptrs: inout [UnsafeMutableRawPointer?]?, _ numPlanes: Int) -> vx_status {
+fileprivate var no_ptrs = [UnsafeMutableRawPointer?]()
+
+public func vxSwapImageHandle(_ image: Image, _ new_ptrs: [UnsafeMutableRawPointer?], _ numPlanes: Int) -> vx_status {
+  var new_ptrs = new_ptrs
+  return Clibvisionworks.vxSwapImageHandle(image.reference, &new_ptrs, nil, vx_size(numPlanes))
+}
+
+public func vxSwapImageHandle(_ image: Image, _ new_ptrs: [UnsafeMutableRawPointer?]?, _ prev_ptrs: inout [UnsafeMutableRawPointer?], _ numPlanes: Int) -> vx_status {
   if var new_ptrs = new_ptrs {
-    if var prev_ptrs = prev_ptrs {
-      return Clibvisionworks.vxSwapImageHandle(image.reference, &new_ptrs, &prev_ptrs, vx_size(numPlanes))
-    } else {
-      return Clibvisionworks.vxSwapImageHandle(image.reference, &new_ptrs, nil, vx_size(numPlanes))
-    }
+    var prev_ptrs = prev_ptrs
+    return Clibvisionworks.vxSwapImageHandle(image.reference, &new_ptrs, &prev_ptrs, vx_size(numPlanes))
   } else {
-    if var prev_ptrs = prev_ptrs {
-      return Clibvisionworks.vxSwapImageHandle(image.reference, nil, &prev_ptrs, vx_size(numPlanes))
-    } else {
-      return Clibvisionworks.vxSwapImageHandle(image.reference, nil, nil, vx_size(numPlanes))
-    }
+    var prev_ptrs = prev_ptrs
+    return Clibvisionworks.vxSwapImageHandle(image.reference, nil, &prev_ptrs, vx_size(numPlanes))
   }
 }
