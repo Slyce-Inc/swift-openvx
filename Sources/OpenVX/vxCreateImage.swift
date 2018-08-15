eabe4b01442ba2ad62532ed593ebb1ac90e83fe3
Jason Allum <jallum@tonicdesign.com>
2018-08-15 13:54:02 -0400

    Added missing deferred release.

--- Sources/OpenVX/vxCreateImage.swift
+++ Sources/OpenVX/vxCreateImage.swift
@@ -5,5 +5,6 @@ public func vxCreateImage(_ context: Context, _ width: Int, _ height: Int, _ typ
   guard let reference = vxCreateImage(context.reference, vx_uint32(width), vx_uint32(height), type.vx_value) else {
     return nil
   }
+  defer { vxReleaseImage(reference) }
   return Image(reference:reference)
 }
