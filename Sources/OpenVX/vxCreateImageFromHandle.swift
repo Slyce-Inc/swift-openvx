46a4971ef3321e4acc66922bace4d92d2a4ad7c5
Jason Allum <jallum@tonicdesign.com>
2018-08-15 13:55:04 -0400

    Added missing release; cleanup.

--- Sources/OpenVX/vxCreateImageFromHandle.swift
+++ Sources/OpenVX/vxCreateImageFromHandle.swift
@@ -1,11 +1,10 @@
 import Clibvisionworks
 
 
-public func vxCreateImageFromHandle(_ context: Context, _ type: ImageType, _ addrs: [vx_imagepatch_addressing_t], _ ptrs: [UnsafeMutableRawPointer?], _ memoryType: MemoryType) -> Image? {
-  var vaddrs = addrs
-  var vptrs = ptrs
-  guard let reference = vxCreateImageFromHandle(context.reference, type.vx_value, &vaddrs, &vptrs, memoryType.vx_value) else {
+public func vxCreateImageFromHandle(_ context: Context, _ type: ImageType, _ addrs: [vx_imagepatch_addressing_t], _ ptrs: [UnsafeMutableRawPointer?]?, _ memoryType: MemoryType) -> Image? {
+  guard let reference = vxCreateImageFromHandle(context.reference, type.vx_value, addrs, ptrs, memoryType.vx_value) else {
     return nil
   }
+  defer { vxReleaseImage(reference) }
   return Image(reference:reference)
 }
