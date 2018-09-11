a0ebe697e5a2b0f795128a4654633336ab55136f
Jason Allum <jallum@tonicdesign.com>
2018-09-11 11:02:55 -0400

    Added RangeThreshold.

--- Sources/OpenVX/Threshold.swift
+++ Sources/OpenVX/Threshold.swift
@@ -48,6 +48,53 @@ public class BinaryThreshold<T:Thresholdable>: Referenceable, Threshold {
   }
 }
 
+public class RangeThreshold<T:Thresholdable>: Referenceable, Threshold {
+  public let reference: vx_threshold
+
+  public required init(reference: vx_threshold) {
+    vxRetainReference(reference)
+    self.reference = reference
+  }
+
+  public init?(context: Context, lowerValue: Int, upperValue: Int, trueValue: Int? = nil, falseValue: Int? = nil) {
+    guard let dataType = DataType.from(type: T.self) else {
+      return nil
+    }
+    guard let reference = vxCreateThreshold(context.reference, VX_THRESHOLD_TYPE_RANGE, dataType.vx_value) else {
+      return nil
+    }
+    defer { vxReleaseThreshold(reference) }
+
+    var lv = Int32(lowerValue)
+    guard VX_SUCCESS == vxSetThresholdAttribute(reference, VX_THRESHOLD_THRESHOLD_LOWER, &lv, MemoryLayout.size(ofValue: lv)) else {
+      return nil
+    }
+    var uv = Int32(upperValue)
+    guard VX_SUCCESS == vxSetThresholdAttribute(reference, VX_THRESHOLD_THRESHOLD_UPPER, &uv, MemoryLayout.size(ofValue: uv)) else {
+      return nil
+    }
+    if let tv = trueValue {
+      var v = Int32(tv)
+      guard VX_SUCCESS == vxSetThresholdAttribute(reference, VX_THRESHOLD_TRUE_VALUE, &v, MemoryLayout.size(ofValue: v)) else {
+        return nil
+      }
+    }
+    if let fv = falseValue {
+      var v = Int32(fv)
+      guard VX_SUCCESS == vxSetThresholdAttribute(reference, VX_THRESHOLD_FALSE_VALUE, &v, MemoryLayout.size(ofValue: v)) else {
+        return nil
+      }
+    }
+
+    self.reference = reference
+    vxRetainReference(reference)
+  }
+
+  deinit {
+    vxReleaseThreshold(reference)
+  }
+}
+
 
 public enum ThresholdType {
 case Binary
